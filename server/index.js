6c3733be890e2be77989b075c741f98e9f54a958
aniket-patil7075 <akp7075@gmail.com>
2024-11-12 22:21:56 +0530

    Initialize project

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 7500;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    credentials:true,
+}))
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+
+mongoose.connect(databaseURL).then(()=>console.log(`DB Connection Successfull`)).catch((err)=>console.log(err.message))
