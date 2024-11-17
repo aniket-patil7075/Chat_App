fefbf0fe76a60fca10a25a40c146e82581e0ba02
Ekata1233 <jadhavjanki8462@gmail.com>
2024-11-17 16:53:19 +0530

    set user information in profile page

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -100,3 +100,37 @@ export const getUserInfo =async (request , response, next )=>{
     }
 
 }
+
+export const updateProfile =async (request , response, next )=>{
+    try{
+        const {userId}=request;
+        const{firstName,lastName,color}=request.body;
+
+       if(!firstName || !lastName){
+        return response.status(404).send("firstname lastname and color is required.")
+
+       }
+
+       const userData =await User.findByIdAndUpdate(userId,{
+        firstName,lastName,color,profileSetup:true
+       },{new:true,runValidators:true})
+       return response.status(200).json({
+        
+            id:userData.id,
+            email:userData.email,
+            profileSetup:userData.profileSetup,
+            firstName:userData.firstName,
+            lastName:userData.lastName,
+            image:userData.iamge,
+            colors:userData.colors
+       
+       })
+
+   
+
+    }catch(error){
+        console.log({error})
+        return response.status(500).send("Internal server error")
+    }
+
+}
